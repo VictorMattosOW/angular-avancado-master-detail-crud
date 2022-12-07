5d31ead3c02684876d6e5f3d5bcf6574cd1b7fd5
Victor Mattos <victor_rm@outlook.com>
2022-12-07 20:20:33 -0300

    melhoria no entryForm

--- src/app/pages/entries/entries.module.ts
+++ src/app/pages/entries/entries.module.ts
@@ -1,3 +1,4 @@
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { EntryFormComponent } from './entry-form/entry-form.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
@@ -5,13 +6,15 @@ import { CommonModule } from '@angular/common';
 import { EntriesRoutingModule } from './entries-routing.module';
 import { EntryListaComponent } from './entry-lista/entry-lista.component';
 import { ReactiveFormsModule } from '@angular/forms';
-
-
+import { IMaskModule } from 'angular-imask';
+import { CalendarModule } from 'primeng/calendar';
 @NgModule({
   imports: [
     CommonModule,
     EntriesRoutingModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    IMaskModule,
+    CalendarModule
   ],
   declarations: [EntryListaComponent, EntryFormComponent],
 })
