0303c399a8e80a436f6bf2abf080e8ee8d66b864
Victor Mattos <victor_rm@outlook.com>
2023-02-02 01:58:26 -0300

    comimit

--- src/app/pages/entries/shared/entry.service.ts
+++ src/app/pages/entries/shared/entry.service.ts
@@ -1,8 +1,9 @@
 import { BaseResourceService } from 'src/app/shared/services/base-resource.service';
-import { Observable, mergeMap } from 'rxjs';
+import { Observable, mergeMap, map } from 'rxjs';
 import { CategoriaService } from './../../categorias/shared/categoria.service';
 import { Injectable, Injector } from '@angular/core';
 import { Entry } from './entry.model';
+import  * as moment from "moment"; 
 
 @Injectable({
   providedIn: 'root'
@@ -20,6 +21,23 @@ export class EntryService extends BaseResourceService<Entry> {
 
   override update(entry: Entry): Observable<Entry> {
     return this.setCategoryAndSentToServer(entry, super.update.bind(this));
+  }
+
+  getByMonthAndYear(month: number, year: number): Observable<Entry[]> {
+    return this.getAll().pipe(
+      map(entries => this.filterByMonthAndYear(entries, month, year))
+    )
+  }
+
+  private filterByMonthAndYear(entries: Entry[], month: number, year: number) {
+    return entries.filter(entry => {
+      const entryDate = moment(entry.data, 'DD/MM/YYYY');
+      const monthMatches = entryDate.month() + 1 === month;
+      const yearMatches = entryDate.year() === year;
+
+      if(monthMatches && yearMatches) return entry;
+      else return null;
+    })
   }
 
   private setCategoryAndSentToServer(entry: Entry, sendFn: (entry: Entry) => Observable<Entry>): Observable<Entry> {
--- src/app/pages/reports/reports/reports.component.ts
+++ src/app/pages/reports/reports/reports.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { CategoriaService } from './../../categorias/shared/categoria.service';
+import { EntryService } from './../../entries/shared/entry.service';
+import { Entry } from './../../entries/shared/entry.model';
+import { Categoria } from './../../categorias/shared/categoria.model';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import currencyFormatter from 'currency-formatter';
 
 @Component({
   selector: 'app-reports',
@@ -7,13 +12,100 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReportsComponent implements OnInit {
 
-  constructor() { }
+  expenseTotal = 0;
+  revenueTotal = 0;
+  balance = 0;
+
+  expenseChartData: any;
+  revenueChartData: any;
+
+  chartOptions = {
+    scales: {
+      yAxes: [
+        {
+          ticks: {
+            beginAtZero: true
+          }
+        }
+      ]
+    }
+  }
+
+  categories: Categoria[] = [];
+  entries: Entry[] = [];
+
+  @ViewChild('month') month: ElementRef = null;
+  @ViewChild('year') year: ElementRef = null;
+
+  constructor(private entryService: EntryService, private categoriaService: CategoriaService) { }
 
   ngOnInit(): void {
+    this.categoriaService.getAll().subscribe({
+      next: (categorias) => {
+        this.categories = categorias;
+      }
+    });
   }
 
   generateReports() {
-    
+    const month = this.month.nativeElement.value;
+    const year = this.year.nativeElement.value;
+
+    if (!month || !year) {
+      alert('Você precisa selecionar o mês e o ano para gerar os relatótios');
+    } else {
+      this.entryService.getByMonthAndYear(month, year).subscribe(this.setValues.bind(this));
+    }
   }
 
+  private setValues(entries: Entry[]) {
+    this.entries = entries;
+    this.calculateBalance();
+    this.setChartData();
+  }
+
+  private calculateBalance() {
+    let expenseTotal = 0;
+    let revenueTotal = 0;
+
+    this.entries.forEach(entry => {
+      if (entry.tipo == 'revenue') {
+        revenueTotal += currencyFormatter.unformat(entry.valor, { code: 'BRL' });
+      } else {
+        revenueTotal += currencyFormatter.unformat(entry.valor, { code: 'BRL' });
+      }
+    });
+
+    this.expenseTotal = currencyFormatter.format(expenseTotal, { code: 'BRL' });
+    this.revenueTotal = currencyFormatter.format(revenueTotal, { code: 'BRL' });
+    this.balance = currencyFormatter.format(revenueTotal - expenseTotal, { code: 'BRL' });
+  }
+
+  private setChartData() {
+    const chartData = [];
+    this.categories.forEach(category => {
+      const filteredEntries = this.entries.filter(
+        entry => (entry.categoriaId == category.id) && (entry.tipo === 'revenue')
+      );
+
+      if (filteredEntries.length > 0) {
+        const totalAmount = filteredEntries.reduce((total, entry) => total + currencyFormatter.unformat(entry.valor, { code: 'BRL' }), 0);
+
+        chartData.push({
+          categoryName: category.nome,
+          totalAmount: totalAmount
+        });
+      }
+    });
+    this.revenueChartData = {
+      labels: chartData.map(item => item.categoryName),
+      datasets: [
+        {
+          label: 'Receitas',
+          backgroundColor: '#9CCC65',
+          data: chartData.map(item => item.totalAmount),
+        }
+      ]
+    }
+  }
 }
